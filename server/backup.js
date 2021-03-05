fccda18aa3887a6aaad06275b13eb407b8748a1a
maicol <mmoarias4@gmail.com>
2021-03-04 19:06:47 -0500

    update server

--- server/backup.js
+++ server/backup.js
@@ -3,9 +3,10 @@ const exec = require('child-process-async').exec;
 var sleep = (seg) => new Promise(res => { setTimeout(() => { res() }, seg * 1000) })
 
 async function backup() {
+    let ip_backup = process.env.IP_BACKUP
     while (true) {
         await sleep(300) // cada 5 min para prueba
-        try { await exec('sh backup.sh') }
+        try { await exec(`sh backup.sh ${ip_backup}`) }
         catch { }
     }
 }
