d198835fc50de264fd8671758bb0ec85c95531c6
maicol <mmoarias4@gmail.com>
2021-03-06 16:36:38 -0500

    test

--- server/backup.js
+++ server/backup.js
@@ -1,20 +1,24 @@
 const exec = require('child-process-async').exec;
 const axios = require('axios');
+
+var ip_backup = process.env.IP_BACKUP
+let url = `${process.env.ROUTE_MIDDLE}/logs/`
+
 var sleep = (seg) => new Promise(res => { setTimeout(() => { res() }, seg * 1000) })
 
 async function backup() {
-    let ip_backup = process.env.IP_BACKUP
-    let ip_middleware=process.env.IP_MIDDLEWARE
     while (true) {
         await sleep(300) // cada 5 min para prueba
-        try { 
-            await exec(`sh backup.sh ${ip_backup}`) 
-            axios.post(`${ip_middleware}info`,{message:"DB backup complete"})
-        }
-        catch {
-            axios.post(`${ip_middleware}error`,{message:"DB backup hasn't been completed"})
-        }
+        try {
+            await exec(`sh backup.sh ${ip_backup}`)
+            sendLogger('DB backup complete', true)
+        } catch { sendLogger("DB backup hasn't been completed", false) }
     }
+}
+
+function sendLogger(msg, success) {
+    try { axios.post(url + success ? 'info' : 'error', { message: msg }) }
+    catch { }
 }
 
 module.exports = backup
