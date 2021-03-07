dfb0dd71d1287887ce021a196bf6167eb8a1467d
maicol <mmoarias4@gmail.com>
2021-03-07 10:17:35 -0500

    .

--- server/backup.js
+++ server/backup.js
@@ -8,16 +8,16 @@ var sleep = (seg) => new Promise(res => { setTimeout(() => { res() }, seg * 1000
 
 async function backup() {
     while (true) {
-        await sleep(300) // cada 5 min para prueba
+        await sleep(180) // cada 3 min para prueba
         try {
             await exec(`sh backup.sh ${ip_backup}`)
-            sendLogger('DB backup complete', true)
-        } catch { sendLogger("DB backup hasn't been completed", false) }
+            sendLogger('DB BACKUP COMPLETE', true)
+        } catch { sendLogger("DB BACKUP HASN'T BEEN COMPLETED", false) }
     }
 }
 
 function sendLogger(msg, success) {
-    try { axios.post(url + success ? 'info' : 'error', { message: msg }) }
+    try { axios.post(url + (success ? 'info' : 'error'), { message: msg }) }
     catch { }
 }
 
